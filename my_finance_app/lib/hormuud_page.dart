531b756ea83c99d2f69a8774aafa6a5071763221
Ali Ahmed Warsame <Ly786691@gmail.com>
2025-01-15 19:20:08 +0300

    Create hormuud_page.dart

---
+++ my_finance_app/lib/hormuud_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HormuudPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hormuud'),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text('Airtime'),
+            subtitle: Text('0 USD'),
+          ),
+          ListTile(
+            title: Text('Data'),
+            subtitle: Text('2.69 GB'),
+          ),
+          // Add more list items as needed
+        ],
+      ),
+    );
+  }
+}
