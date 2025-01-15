978897d6dbaa334a3ef801f059c7b3ef94c208b6
Ali Ahmed Warsame <Ly786691@gmail.com>
2025-01-15 19:19:59 +0300

    Create bulsho_page.dart

---
+++ my_finance_app/lib/bulsho_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BulshoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bulsho'),
+      ),
+      body: Center(
+        child: Text('Bulsho Page Content'),
+      ),
+    );
+  }
+}
