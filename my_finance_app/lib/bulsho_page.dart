6502bc5052cf4fcd15145acb0c34dff8886f3683
Ali Ahmed Warsame <Ly786691@gmail.com>
2025-01-15 19:48:40 +0300

    Update bulsho_page.dart

--- my_finance_app/lib/bulsho_page.dart
+++ my_finance_app/lib/bulsho_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class BulshoPage extends StatelessWidget {
+  const BulshoPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
