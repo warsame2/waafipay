bcb377c6e04fa78a2d8fbb9daf46da481c79e30c
Ali Ahmed Warsame <Ly786691@gmail.com>
2025-01-15 19:48:53 +0300

    Update hormuud_page.dart

--- my_finance_app/lib/hormuud_page.dart
+++ my_finance_app/lib/hormuud_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HormuudPage extends StatelessWidget {
+  const HormuudPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
