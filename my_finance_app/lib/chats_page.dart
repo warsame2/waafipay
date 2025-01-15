19f75cfb7ce271d9417a0ae7e8678febc0afee98
Ali Ahmed Warsame <Ly786691@gmail.com>
2025-01-15 19:20:04 +0300

    Create chats_page.dart

---
+++ my_finance_app/lib/chats_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chats'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('WAAFI 12 Jan'),
+            subtitle: Text('WAAFI DIGITAL CARD 4'),
+          ),
+          ListTile(
+            title: Text('SALAAM BANK 05.12.24'),
+            subtitle: Text('SB 25.00 USD, AYAA LAGU WAREEJIYAY KONTADAADA 359XXXX42. KANA TIMID'),
+          ),
+          // Add more list items as needed
+        ],
+      ),
+    );
+  }
+}
